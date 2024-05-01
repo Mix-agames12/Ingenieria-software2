a732855c08ccd392f9cff3c49dec90bfe5637f5f
Leonardo <leonardo.quishpe@epn.edu.ec>
2024-05-01 08:06:15 -0500

    Cambios Leo :D

--- calculadora.py
+++ calculadora.py
@@ -29,8 +29,14 @@ calcular()
 def suma(a, b):
     return a + b
 
-def resta (a, b):
-    return a-b
+def resta(a, b):
+    return a - b
 
 def multiplicacion(a, b):
     return a * b
+
+def division(a, b):
+    if b == 0:
+        return "Error: No se puede dividir por cero."
+    else:
+        return a / b
