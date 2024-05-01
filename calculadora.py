5d91ae14d42bdf1179815745bd316b2d861fb9bc
rarmendarizc <rossy.armendariz@epn.edu.ec>
2024-05-01 09:27:45 -0500

    Agrego potencia

--- calculadora.py
+++ calculadora.py
@@ -28,3 +28,18 @@ calcular()
 
 def suma(a, b):
     return a + b
+
+def resta(a, b):
+    return a - b
+
+def potencia(a, b):
+    return a ** b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        return "Error: No se puede dividir por cero."
+    else:
+        return a / b
