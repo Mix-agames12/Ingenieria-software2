7804d101aa4035af4d9de1555682e3b206f9ffd7
domepl <dome.plaza456@gmail.com>
2024-05-01 00:30:36 -0500

    Se implemento funcion resta

---
+++ calculadora.py
@@ -0,0 +1,30 @@
+def calcular():
+    print("Selecciona la operación:")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Potencia")
+
+    opcion = input("Ingresa el número de la operación deseada: ")
+
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+
+    if opcion == '1':
+        print("Resultado:", suma(num1, num2))
+    elif opcion == '2':
+        print("Resultado:", resta(num1, num2))
+    elif opcion == '3':
+        print("Resultado:", multiplicacion(num1, num2))
+    elif opcion == '4':
+        print("Resultado:", division(num1, num2))
+    elif opcion == '5':
+        print("Resultado:", potencia(num1, num2))
+    else:
+        print("Opción no válida")
+
+calcular()
+
+def resta (a, b):
+    return a-b
